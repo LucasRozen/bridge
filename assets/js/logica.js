d2c1c95f8a3c3da691db815faf2c9bcec0137ed1
agusborsa <agustinborsaumai@gmail.com>
2021-09-17 17:47:16 -0300

    Prueba js ul reproductor big

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -9,7 +9,7 @@ var ul = document.getElementById("loop_lista_de_canciones_reproductor_big")
         <li>
         <h3><span>${i+1}.</span>${canciones[i].nombre}</h3><span>${canciones[i].duracion}</span>
         <a class="reproducir_pausar_reproductor_big">
-           <i class="fas fa-play"><audio src="https://bridge217.qodeinteractive.com/wp-content/uploads/2018/04/Black-hole-sun.mp3"></audio></i>
+            <i class="fas fa-play"><audio src=""></audio></i>
         </a>
         <a class="video_reproductor_big" href="https://vimeo.com/33730560">
             <i class="fa fa-video-camera" aria-hidden="true"></i>
