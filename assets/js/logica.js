a9e732e4226860a9defb976b8a2c1a5c366f1e21
agusborsa <agustinborsaumai@gmail.com>
2021-09-22 21:19:06 -0300

    prueba ul1

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,10 +1,10 @@
-var ul = document.getElementById("loop_lista_de_canciones_reproductor_big");
+var ul1 = document.getElementById("loop_lista_de_canciones_reproductor_big");
 
         var canciones=[{nombre:"Black Hole Sun",duracion: "0:32"},{nombre:"Work",duracion: "0:27"},{nombre:"Lullaby",duracion: "0:30"},{nombre:"The Passanger",duracion: "0:30"},{nombre:"Celebrity Skin",duracion: "0:32"},{nombre:"Creep",duracion: "0:30"},{nombre:"Epic",duracion: "0:30"},{nombre:"Reptilia",duracion: "0:30"},{nombre:"Go With A Flow",duracion: "0:30"},{nombre:"Electric Version",duracion: "0:31"}];
 
         for (i=0;i<10;i++) {
 
-        ul.innerHTML+=`
+        ul1.innerHTML+=`
 
         <li>
         <h3><span>${i+1}.</span>${canciones[i].nombre}</h3><span>${canciones[i].duracion}</span>
